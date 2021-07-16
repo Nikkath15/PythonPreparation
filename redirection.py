09f1409e79b33fee9676bf97bbb5b2e1873d2c1a
Nikkath <writetonikkath@gmail.com>
2021-07-16 18:57:42 +0530

    Second COmmit

--- redirection.py
+++ redirection.py
@@ -1,3 +1,3 @@
 a=input("enter a string:")
 b=input("Enter another string:")
-print("Both a and b string are:" , repr(a) ,"and", repr(b))
+print("Both a and b string are:" , repr(a) ,"and ", repr(b))
